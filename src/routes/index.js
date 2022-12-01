a725bee2cd1191b9dd2ad2b072aae33f72a493a2
ShinCoder <88935125+ShinCoder@users.noreply.github.com>
2022-12-01 13:40:39 +0700

    Course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const coursesRouter = require('./courses');
+
 function route(app) {
     app.use('/', siteRouter);
 
     app.use('/news', newsRouter);
 
+    app.use('/courses', coursesRouter);
 }
 
 module.exports = route;
